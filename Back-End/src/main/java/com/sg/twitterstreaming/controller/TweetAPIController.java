53c7b9b487efb5c483aaa3bf9dce4fdcbcbaa316
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-10-06 17:47:19 +0530

    configure backend

--- Back-End/src/main/java/com/sg/twitterstreaming/controller/TweetAPIController.java
+++ Back-End/src/main/java/com/sg/twitterstreaming/controller/TweetAPIController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/tweets/")
 public class TweetAPIController {
 
--- Back-End/src/main/java/com/sg/twitterstreaming/controller/TweetDBController.java
+++ Back-End/src/main/java/com/sg/twitterstreaming/controller/TweetDBController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/tweets/offline")
 public class TweetDBController {
 
