2607388d0f8fa53a5d16f0f6ec83444339e3a764
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-10-15 13:23:02 +0530

    refactor for heroku-deploy

--- Back-End/src/main/java/com/sg/twitterstreaming/TwitterStreamingApplication.java
+++ Back-End/src/main/java/com/sg/twitterstreaming/TwitterStreamingApplication.java
@@ -6,7 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TwitterStreamingApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        String ENV_PORT = System.getenv().get("PORT");
+        String ENV_DYNO = System.getenv().get("DYNO");
+        if(ENV_PORT != null && ENV_DYNO != null) {
+            System.getProperties().put("server.port", ENV_PORT);
+        }
+        
         SpringApplication.run(TwitterStreamingApplication.class, args);
     }
 
